b7e6fda1b4b857af7c4f8303b539bdd509698870
Henry Cummings <harry@henry-cummings-computer.local>
2013-09-12 14:53:10 +0100

    Move keyboard handling into input module

--- client/views/input.js
+++ client/views/input.js
@@ -1,15 +1,33 @@
 'use strict';
 
+var keyMap = {
+    87: 0,
+    38: 0,
+    68: 1,
+    39: 1,
+    83: 2,
+    40: 2,
+    65: 3,
+    37: 3
+};
+
+function directionFromKey(keyCode) {
+    if (keyMap.hasOwnProperty(keyCode.toString())) {
+        return keyMap[keyCode];
+    } else {
+        return null;
+    }
+}
+
 module.exports = function build(arena, placeArrowCallback) {
     var constants = require('../graphics/constants.js');
-    var direction = require('../../shared/utils/direction.js');
 
     var activeKey;
     
     window.oncontextmenu = function() { return false; };
 
     window.onkeydown = function (event) {
-        if (direction.fromKey(event.keyCode) !== null) {
+        if (directionFromKey(event.keyCode) !== null) {
             activeKey = event.keyCode;
             event.preventDefault();
         }
@@ -36,7 +54,7 @@ module.exports = function build(arena, placeArrowCallback) {
         placeArrowCallback(
             Math.floor((event.clientX - offsetX) / constants.CELL_SIZE),
             Math.floor((event.clientY - offsetY) / constants.CELL_SIZE),
-            direction.fromKey(activeKey)
+            directionFromKey(activeKey)
         );
     };
 };
