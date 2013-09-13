ca5b061f5b160a5195a4f7cc1b01e80b42a94cb6
hgcummings <harry@xlr3.com>
2013-09-13 23:44:32 +0100

    Compensate for scrolling when placing arrows Make instructions clearer

--- client/views/input.js
+++ client/views/input.js
@@ -28,15 +28,39 @@ function clearHandlers(arena) {
     window.oncontextmenu = null;
     window.onkeydown = null;
     window.onkeyup = null;
+    window.onresize = null;
     arena.onmousedown = null;
     arena.onmousemove = null;
 }
 
-methods.desktop = function desktop(arena, placeArrowCallback) {
-    var activeKey;
-
+function offsetHandler(arena) {
     var offsetX;
     var offsetY;
+
+    window.onresize = function() {
+        offsetX = null;
+        offsetY = null;
+    };
+
+    function getRelativeX(event) {
+        offsetX = offsetX || arena.offsetLeft + (arena.offsetWidth - arena.width) / 2;
+        return event.clientX - offsetX + document.body.scrollLeft;
+    }
+
+    function getRelativeY(event) {
+        offsetY = offsetY || arena.offsetTop + (arena.offsetHeight - arena.height) / 2;
+        return event.clientY - offsetY + document.body.scrollTop;
+    }
+
+    return {
+        getRelativeX: getRelativeX,
+        getRelativeY: getRelativeY
+    };
+}
+
+methods.desktop = function desktop(arena, placeArrowCallback) {
+    var activeKey;
+    var handler = offsetHandler(arena);
 
     window.oncontextmenu = function() { return false; };
 
@@ -64,12 +88,9 @@ methods.desktop = function desktop(arena, placeArrowCallback) {
             return;
         }
 
-        offsetX = offsetX || arena.offsetLeft + (arena.offsetWidth - arena.width) / 2;
-        offsetY = offsetY || arena.offsetTop + (arena.offsetHeight - arena.height) / 2;
-
         placeArrowCallback(
-            Math.floor((event.clientX - offsetX) / constants.CELL_SIZE),
-            Math.floor((event.clientY - offsetY) / constants.CELL_SIZE),
+            Math.floor(handler.getRelativeX(event) / constants.CELL_SIZE),
+            Math.floor(handler.getRelativeY(event) / constants.CELL_SIZE),
             direction
         );
     };
@@ -81,18 +102,13 @@ methods.desktop = function desktop(arena, placeArrowCallback) {
 
 methods.laptop = function desktop(arena, placeArrowCallback) {
     var currentLocation = {};
-
-    var offsetX;
-    var offsetY;
+    var handler = offsetHandler(arena);
 
     window.oncontextmenu = function() { return false; };
 
     arena.onmousemove = function(event) {
-        offsetX = offsetX || arena.offsetLeft + (arena.offsetWidth - arena.width) / 2;
-        offsetY = offsetY || arena.offsetTop + (arena.offsetHeight - arena.height) / 2;
-
-        currentLocation.x = event.clientX - offsetX;
-        currentLocation.y = event.clientY - offsetY;
+        currentLocation.x = handler.getRelativeX(event);
+        currentLocation.y = handler.getRelativeY(event);
     };
 
     window.onkeydown = function(event) {
