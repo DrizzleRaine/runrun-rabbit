3dd571bd0e3f045767f4f30c4741a0f790f62e3e
hgcummings <harry@xlr3.com>
2013-10-06 22:02:11 +0100

    Fix loglevel dependency in package.json Revert commit of four-player spike code

--- shared/levels.js
+++ shared/levels.js
@@ -16,18 +16,18 @@ function level(sources, sinks) {
 }
 
 levels[0] = level([new Source(0, 3, 1), new Source(11, 6, 3)],
-    [new Sink(0, 11, 3), new Sink(1, 0, 6), new Sink(2, 6, 0), new Sink(3, 5, 9)]);
+    [new Sink(0, 11, 3), new Sink(1, 0, 6), new Sink(null, 6, 0), new Sink(null, 5, 9)]);
 
 levels[1] = level(
     [new Source(5, 0, 2), new Source(6, 9, 0), new Source(6, 0, 2), new Source(5, 9, 0)],
-    [new Sink(0, 1, 0), new Sink(2, 0, 1), new Sink(null, 2, 0), new Sink(null, 0, 2),
-     new Sink(1, 11, 8), new Sink(3, 10, 9), new Sink(null, 11, 7), new Sink(null, 9, 9)]);
+    [new Sink(0, 1, 0), new Sink(0, 0, 1), new Sink(null, 2, 0), new Sink(null, 0, 2),
+        new Sink(1, 11, 8), new Sink(1, 10, 9), new Sink(null, 11, 7), new Sink(null, 9, 9)]);
 
 levels[2] = level([new Source(0, 8, 1), new Source(11, 9, 3)],
     [new Sink(null, 0, 7), new Sink(null, 0, 0), new Sink(null, 11, 0),
-     new Sink(0, 2, 0), new Sink(1, 4, 0), new Sink(2, 7, 0), new Sink(3, 9, 0)]);
+        new Sink(0, 2, 0), new Sink(1, 4, 0), new Sink(0, 7, 0), new Sink(1, 9, 0)]);
 
 levels[3] = level([new Source(5, 3, 0), new Source(6, 3, 1), new Source (6, 6, 2), new Source(5, 6, 3)],
-    [new Sink(0, 2, 2), new Sink(1, 9, 2), new Sink(2, 9, 7), new Sink(3, 2, 7)]);
+    [new Sink(0, 2, 2), new Sink(1, 9, 2), new Sink(0, 9, 7), new Sink(1, 2, 7)]);
 
 module.exports = levels;
