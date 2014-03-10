6f50b481bffcee5cc9f2a1f26647c5be2c871a51
hgcummings <harry@xlr3.com>
2014-03-10 23:49:35 +0000

    Simplify main.js using data binding

--- client/main.js
+++ client/main.js
@@ -1,89 +1,70 @@
 'use strict';
 
-var newVisitor = !localStorage.visited;
-localStorage.visited = true;
+var container = document.getElementById('container');
+var gameController = require('./controllers/game.js');
+var banner = require('./views/banner.js')(container);
+banner.show();
 
+// Setup model
+var model = {
+    menu: true,
+    hintMessage: '',
+    options: {},
+    returnToMenu: function (event) {
+        model.optionsControllers.forEach(function(controller) {
+            if (controller.open) {
+                controller.hide();
+                showMenu();
+                event.preventDefault();
+            }
+        });
+    },
+    showController: function (event) {
+        hideMenu();
+        model[this.getAttribute('data-controller')].show();
+        event.preventDefault();
+    },
+    startGame: function(event) {
+        hideMenu();
+        gameController.init(this.getAttribute('value') === 'true', model.options);
+        event.preventDefault();
+    }
+};
+
+function showMenu() {
+    model.menu = true;
+    banner.show();
+}
+
+function hideMenu() {
+    model.menu = false;
+    banner.hide();
+}
+
+model.settings = require('./controllers/settings.js')(model.options);
+model.accessibility = require('./controllers/accessibility.js')(model.options);
+model.optionsControllers = [model.settings, model.accessibility];
+
+// Data-bind model and view
 var rivets = require('rivets');
 rivets.configure({
     prefix: 'data'
 });
-
-var gameController = require('./controllers/game.js');
-var hintMessage = require('./views/message.js').build(document.getElementById('container'), 'menuHint');
-var banner = require('./views/banner.js')(document.getElementById('container'));
-banner.show();
-
-var options = {};
-var settings = require('./controllers/settings.js')(options);
-var accessibility = require('./controllers/accessibility.js')(options);
-var optionsControllers = [settings, accessibility];
-
-rivets.bind(document.getElementById('settings'), settings);
-rivets.bind(document.getElementById('accessibility'), accessibility);
-
-var hideMenu = function hideMenu() {
-    document.getElementById('menu').classList.add('hidden');
-    hintMessage.view.classList.add('hidden');
-    document.getElementById('backToMenu').classList.remove('hidden');
-    banner.hide();
-};
-
-var showMenu = function showMenu() {
-    document.getElementById('menu').classList.remove('hidden');
-    hintMessage.view.classList.remove('hidden');
-    document.getElementById('backToMenu').classList.add('hidden');
-    banner.show();
-};
-
-function setupHintMessage(elementId, message) {
-    var element = document.getElementById(elementId);
-    element.onmouseover = function() { hintMessage.setText(message); };
-    element.onmouseout = function() { hintMessage.setText(''); };
-}
-
-setupHintMessage('startSinglePlayer',
-    'Start a single player game against a computer-controlled opponent');
-
-setupHintMessage('startMultiplayer',
-    'Start a multiplayer game against a randomly chosen human opponent');
-
-setupHintMessage('settingsAndInstructions',
-    'View instructions and configure input options');
-
-setupHintMessage('accessibilityOptions',
-    'Configure display options for accessibility, and audio volumes');
-
-document.getElementById('startSinglePlayer').onclick = function() {
-    hideMenu();
-    gameController.init(false, options);
-};
-
-document.getElementById('startMultiplayer').onclick = function() {
-    hideMenu();
-    gameController.init(true, options);
-};
-
-var openController = function(controller) {
-    hideMenu();
-    controller.show();
-};
-
-document.getElementById('settingsAndInstructions').onclick = function() { openController(settings); };
-document.getElementById('accessibilityOptions').onclick = function() { openController(accessibility); };
-
-if (newVisitor) {
-    openController(settings);
-}
-
-function returnToMenu(event) {
-    optionsControllers.forEach(function(controller) {
-        if (controller.open) {
-            controller.hide();
-            showMenu();
-            event.preventDefault();
+rivets.bind(container, model, {
+    binders: {
+        'hint-text': {
+            bind: function(element) {
+                element.onmouseover = function() { model.hintMessage = element.getAttribute('data-hint-text'); };
+                element.onmouseout = function() { model.hintMessage = ''; };
+            }
         }
-    });
-}
+    }
+});
 
-document.getElementById('backToMenu').onclick = returnToMenu;
-document.getElementById('startPlaying').onclick = returnToMenu;
+// Take new visitors straight to the instructions/settings view
+var newVisitor = !localStorage.visited;
+localStorage.visited = true;
+if (newVisitor) {
+    model.hideMenu();
+    model.settings.show();
+}
