3daab61b6a5f2e29fc8040a1f59eb4681dba46df
HGC <harry.cummings@softwire.com>
2014-03-12 22:27:27 +0000

    User repository: Expire users on create, extend expiration, fetch details

--- server/repositories/user.js
+++ server/repositories/user.js
@@ -4,19 +4,26 @@ var uuid = require('node-uuid');
 var promise = require('promise');
 
 module.exports.build = function buildUserRepo(redisClient) {
+    // Lifetime of user accounts associated only with a cookie (rather than, for example, an OpenID token)
+    var UNAUTHENTICATED_USER_EXPIRY = 86400;
+
     var hsetnx = promise.denodeify(redisClient.hsetnx);
     var hset = promise.denodeify(redisClient.hset);
     var hget = promise.denodeify(redisClient.hget);
+    var hgetall = promise.denodeify(redisClient.hgetall);
     var exists = promise.denodeify(redisClient.exists);
+    var expire = promise.denodeify(redisClient.expire);
 
     var userRepo = {
         createUser: function(username, callback) {
             var userId = 'player:' + uuid.v4();
 
             var tryAddUsername = hsetnx('names', username, userId);
-            var saveUser = hset(userId, 'name', username).then(function() {
-                return userId;
-            });
+            var saveUser = hset(userId, 'name', username)
+                .then(expire(userId, 1800))
+                .then(function() {
+                    return userId;
+                });
             var tryStealUsername =
                 hget('names', username)
                 .then(exists)
@@ -37,6 +44,12 @@ module.exports.build = function buildUserRepo(redisClient) {
                     }
                 })
                 .nodeify(callback);
+        },
+        fetchUser: function (userId, callback) {
+            return hgetall(userId).nodeify(callback);
+        },
+        extendUser: function(userId, callback) {
+            return expire(userId, UNAUTHENTICATED_USER_EXPIRY).nodeify(callback);
         }
     };
 
--- test/server/repositories/user.js
+++ test/server/repositories/user.js
@@ -24,11 +24,14 @@ describe('User repository', function() {
         });
     });
 
-    it('should persist new users', function(done) {
+    it('should persist new users with a default TTL', function(done) {
         var username = 'User1';
 
         userRepository.createUser(username)
             .then(function (userId) {
+                var ttl = mockRedis.storage[userId].expires;
+                assert.isTrue(ttl > 0, 'Expected TTL ' + ttl + ' to be > 0');
+                assert.isTrue(ttl <= 1800, 'Expected TTL ' + ttl + 'to be <= 1800');
                 return hget(userId, 'name');
             })
             .then(function(actualUsername) {
@@ -64,6 +67,33 @@ describe('User repository', function() {
             })
             .then(function(actualUsername) {
                 assert.equal(actualUsername, username);
+                done();
+            })
+            .done();
+    });
+
+    it('should return user details for an existing user', function(done) {
+        var username = 'User1';
+
+        userRepository.createUser(username)
+            .then(function (userId) {
+                return userRepository.fetchUser(userId);
+            })
+            .then(function (user) {
+                assert.equal(user.name, username);
+                done();
+            })
+            .done();
+    });
+
+    it('should extend user expiry time', function(done) {
+        userRepository.createUser('user1')
+            .then(function (userId) {
+                userRepository.extendUser(userId);
+                return userId;
+            }).then(function (userId) {
+                var ttl = mockRedis.storage[userId].expires;
+                assert.isTrue(ttl > 86000, 'Expected TTL ' + ttl + ' to be > 86000');
                 done();
             })
             .done();
