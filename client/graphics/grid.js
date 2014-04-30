f8aaae8c8e8c7d502d267c640100c69c045a356c
hgcummings <harry@xlr3.com>
2014-05-01 00:01:14 +0100

    Allow background to scale with grid

--- client/graphics/grid.js
+++ client/graphics/grid.js
@@ -9,13 +9,21 @@ module.exports = function(model) {
     var canvas = document.createElement('canvas');
     canvas.setAttribute('width', width.toString());
     canvas.setAttribute('height', height.toString());
-    canvas.style.backgroundImage = 'url("/background.png")';
+    canvas.style.backgroundColor = '#CCCCCC';
     canvas.classList.add('arena');
 
     var context = canvas.getContext('2d');
 
     var clear = function clear() {
         context.clearRect(0,0, width, height);
+        context.fillStyle = '#EEEEEE';
+        for (var i = 0; i < model.level.width; ++i) {
+            for (var j = 0; j < model.level.height; ++j) {
+                if (i % 2 !== j % 2) {
+                    context.fillRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+        }
     };
 
     return {
