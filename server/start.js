36978ea08e8a2dcdc6664b06c0a8be92f4a16dce
hgcummings <harry@xlr3.com>
2014-09-14 08:32:15 +0100

    Add logging

--- server/start.js
+++ server/start.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var server = require('./server.js');
+console.info('Server starting...');
 server.start(function() {
-    console.info('Server started...');
+    console.info('... Server started.');
 });
